d303bb3e127c672c12fe24d656d6f76d37b882c8
Syariful Kamaluddin <syarifulkamaluddin@gmail.com>
2023-05-06 19:38:32 +0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: FC<NavbarProps> = ({ }) => {
                     <svg className="w-10 h-10" version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" overflow="visible" xmlSpace="preserve">
                         <path className="fill-base-content" d="m305.8 389.08 27.92-158.34h-64.74l-27.92 158.34M222.47 200l16.05-91h-64.74l-16.08 91.2" />
                     </svg>
-                    PasswordReverser
+                    WordReverser
                 </Link>
             </div>
 
